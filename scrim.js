b5556552bbc0bfe47ace535688bec9f2971973b4
Stephen Salem Ibitomi <38494567+peter7506@users.noreply.github.com>
2022-05-29 18:51:28 +0000

    Add files via upload

---
+++ scrim.js
@@ -0,0 +1,58 @@
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+
+// console.log(saveEl)
+
+// function increment() {
+//count += 1
+// countEl.textContent = count
+
+// }
+
+// function save() {
+// let countStr = count + " - "
+// saveEL.textContent += countStr
+// countEl.textContent = 0
+// count = 0
+
+// }
+
+
+// let name = "per"
+// let greeting = "ibi you are welcome"
+
+// function welcomeMessage() {
+//console.log(name + ", " + greeting + "!!")
+// }
+
+// welcomeMessage()
+
+
+// let myPoints = 3
+
+// function add3Points() {
+// myPoints += 3
+// }
+
+// function remove1Point() {
+//myPoints -= 1
+// }
+
+// add3Points()
+// add3Points()
+//add3Points()
+// remove1Point()
+// remove1Point()
+
+// console.log(myPoints)
+
+
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
+
+function purchase() {
+    console.log("button clicked")
+    errorParagraph.textContent = "Sorry, please try again"
+}
